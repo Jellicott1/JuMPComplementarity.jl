f2a95df3e91d5a2d8826cc3673d6df6631b20182
James Ellicott <jellicott21@gmail.com>
2018-08-13 12:25:17 +0100

    initial commit: Added basical Solver struct to JuMPComplementarity.jl as well as adding MathProgBase and AmplNLWriter to REQUIRE.

--- src/JuMPComplementarity.jl
+++ src/JuMPComplementarity.jl
@@ -1,5 +1,22 @@
 module JuMPComplementarity
 
-# package code goes here
+using MathProgBase, AmplNLWriter
+
+export AmplComplementaritySolver
+
+struct AmplComplementaritySolver <: MathProgBase.AbstractMathProgSolver
+    solver_command::String
+    options::Vector{String}
+    filename::String
+end
+
+function AmplComplementaritySolver(solver_command::String, options::Vector{String}=String[]; filename::String="")
+    AmPLComplementaritySolver(solver_command, options, filename)
+end
+#=
+function printModel(m::)
+
+end
+=#
 
 end # module
